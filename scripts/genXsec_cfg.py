9d54185d0691b9b699debd89a1bd5c9e00db4225
Seth Cooper <seth.cooper@cern.ch>
2023-10-05 15:41:33 +0200

    update to newer cmssw

--- scripts/genXsec_cfg.py
+++ scripts/genXsec_cfg.py
@@ -1,14 +1,17 @@
+# Instructions:
+# 1) Need CMSSW_12 or higher release area
+# 2) cmsenv inside release area
+# 3) if needed (first time only), install DBS client with 'pip3 install dbs3-client'
+# 4) run this script: 'cmsRun genXsec_cfg.py dataset=/Full/NanoAOD/dataset'
 import FWCore.ParameterSet.Config as cms
 from FWCore.ParameterSet.VarParsing import VarParsing
 try:
-    import CRABClient
     from dbs.apis.dbsClient import DbsApi
     from dbs.exceptions.dbsClientException import dbsClientException
 except ImportError:
-    print
-    print "ERROR: Could not load dbs APIs.  Please source the crab3 setup:"
-    # print "source /cvmfs/cms.cern.ch/crab3/crab.sh"
-    print("source /cvmfs/cms.cern.ch/common/crab-setup.sh")
+    print()
+    print("ERROR: Could not load dbs APIs")
+    print("You need to run this script using cmsRun after doing 'cmsenv' inside a CMSSW release area with major version >= 12, and then (if necessary-first time only) installing the DBS client with 'pip3 install dbs3-client'.")
     exit(-1)
 
 options = VarParsing ('analysis')
@@ -22,9 +25,9 @@ options.parseArguments()
 
 datasetpath = options.dataset
 if datasetpath=='default':
-    print 'ERROR: must specify full dataset name of NANOAODSIM dataset: dataset=/Primary/Secondary/NANOAODSIM or dataset=test to skip DBS querying'
-    print 'Exiting.'
-    print
+    print('ERROR: must specify full dataset name of NANOAODSIM dataset: dataset=/Primary/Secondary/NANOAODSIM or dataset=test to skip DBS querying')
+    print( 'Exiting.')
+    print()
     exit(-1)
 
 doQuery = True
@@ -33,46 +36,47 @@ if datasetpath=='test':
 
 if doQuery:
     if 'MINIAOD' not in datasetpath:
-        print 'DBS: query for parent of dataset='+datasetpath+'...',
+        print('DBS: query for parent of dataset='+datasetpath+'...', end='')
         
         try:
             api = DbsApi(url = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader')
             parents = api.listDatasetParents(dataset=datasetpath)
-        except dbsClientException, ex:
-            print
-            print "Caught API Exception %s: %s "  % (ex.name,ex)
+        except dbsClientException as ex:
+            print()
+            print("Caught API Exception %s: %s "  % (ex.name,ex))
             exit(-1)
         
         #for parent in parents:
         #    print parent
         if len(parents) > 1:
-            print
-            print 'ERROR: got multiple parents for dataset:'
+            print()
+            print('ERROR: got multiple parents for dataset:')
             for parent in parents:
-                print parent['parent_dataset']
-            print 'ERROR: not sure how to continue.'
+                print(parent['parent_dataset'])
+            print('ERROR: not sure how to continue.')
             exit(-2)
-        print 'Done'
+        print('Done')
         parentDataset = parents[0]['parent_dataset']
-        print 'DBS: Found parent dataset='+parentDataset
+        print('DBS: Found parent dataset='+parentDataset)
     else:
         parentDataset = datasetpath
-    print 'DBS: query for files in dataset...',
+    print('DBS: query for files in dataset...', end ='')
     
     try:
         api = DbsApi(url = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader')
         #dbsFiles = api.listFiles(dataset=datasetpath)
         dbsFiles = api.listFiles(dataset=parentDataset)
-    except dbsClientException, ex:
-        print
-        print "Caught API Exception %s: %s "  % (ex.name,ex)
+    except dbsClientException as ex:
+        print()
+        print("Caught API Exception %s: %s "  % (ex.name,ex))
         exit(-3)
     
-    print 'Done'
+    print('Done')
     #for dbsFile in dbsFiles:
     #    print dbsFile['logical_file_name']
     # get first N LFNs
-    filesToUse = 100
+    # filesToUse = 100
+    filesToUse = 25
     firstNFiles = [dbsFile['logical_file_name'] for dbsFile in dbsFiles]
     # if needed
     filesToSkip = ["A5BFC697-3A86-9A4D-B090-01D0291ADDDD"]
@@ -82,12 +86,12 @@ if doQuery:
     if len(firstNFiles) > filesToUse:
         firstNFiles = firstNFiles[-1*filesToUse:]
     # print firstNFiles
-    print 'INFO: using last',len(firstNFiles),'out of',len(dbsFiles),'files in parent dataset as input to GenXSecAnalyzer'
+    print('INFO: using last',len(firstNFiles),'out of',len(dbsFiles),'files in parent dataset as input to GenXSecAnalyzer')
 else:
-    print "INFO: not doing DBS query."
+    print("INFO: not doing DBS query.")
 
-print 'Run.'
-print
+print('Run.')
+print()
 
 process = cms.Process('XSec')
 
