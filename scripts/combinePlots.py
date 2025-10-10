85a26da18a4108d759eca61f9ddd67ae2dae9740
Seth Cooper <seth.cooper@cern.ch>
2025-10-10 13:52:28 +0200

    bugfix

--- scripts/combinePlots.py
+++ scripts/combinePlots.py
@@ -270,7 +270,7 @@ def MakeCombinedSample(args):
                         thisPieceTable = combineCommon.UpdateTable(dataThisFile, thisPieceTable)
 
                     if not options.tablesOnly:
-                        histoDictThisSample, _ = combineCommon.UpdateHistoDict(histoDictThisSample, sampleHistos, matchingPiece, sample, plotWeight, corrLHESysts, not isMC, isQCD)
+                        histoDictThisSample = combineCommon.UpdateHistoDict(histoDictThisSample, sampleHistos, matchingPiece, sample, plotWeight, corrLHESysts, not isMC, isQCD)
 
                 if not doWeightingThisFile:
                     doPDFReweight = False
